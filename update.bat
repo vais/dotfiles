1855709a85eacbc1807d08fd21ea8c905954b2d9
Vais Salikhov <vsalikhov@gmail.com>
2015-10-23 13:21:54 +0700

    Add gitconfig

--- update.bat
+++ update.bat
@@ -1,3 +1,6 @@
+mklink "%HOMEPATH%\.gitconfig" "%HOMEPATH%\dotfiles\gitconfig"
+mklink "%HOMEPATH%\.gitignore_global" "%HOMEPATH%\dotfiles\gitignore_global"
+
 mklink "%HOMEPATH%\_vimrc" "%HOMEPATH%\dotfiles\vimrc"
 mklink "%HOMEPATH%\vimfiles" "%HOMEPATH%\dotfiles\vimfiles"
 
