7207423e8db68e2f63986f490827a39bcf70be43
Vais Salikhov <vsalikhov@gmail.com>
2017-10-06 08:55:42 +0700

    Add .irbrc and vim abbr for binding.irb

--- update.bat
+++ update.bat
@@ -4,6 +4,8 @@ mklink "%HOMEPATH%\.gitignore_global" "%HOMEPATH%\dotfiles\gitignore_global"
 mklink "%HOMEPATH%\_vimrc" "%HOMEPATH%\dotfiles\vimrc"
 mklink "%HOMEPATH%\vimfiles" "%HOMEPATH%\dotfiles\vimfiles"
 
+mklink "%HOMEPATH%\.irbrc" "%HOMEPATH%\dotfiles\irbrc"
+
 pushd "%HOMEPATH%\dotfiles"
 git pull
 git submodule update --init
--- vimfiles/after/ftplugin/ruby.vim
+++ vimfiles/after/ftplugin/ruby.vim
@@ -4,3 +4,4 @@ nnoremap <buffer> <silent> <F5> :update<Bar>exe ":silent lmake!"<Bar>lwindow5<CR
 imap <buffer> <silent> <F5> <Esc><F5>
 
 iabbr <buffer> pry require 'pry'; binding.pry 
+iabbr <buffer> irb require 'irb'; binding.irb 
