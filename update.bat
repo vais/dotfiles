e0d858fbba64bcb0155a24ab9a8326764598cce5
Vais Salikhov <vsalikhov@gmail.com>
2019-11-06 09:57:38 -0500

    Use git commands without changing current working directory

--- update.bat
+++ update.bat
@@ -1,7 +1,5 @@
-pushd "%HOMEPATH%\dotfiles"
-git pull
-git submodule update --init
-popd
+git -C "%HOMEPATH%\dotfiles" pull
+git -C "%HOMEPATH%\dotfiles" submodule update --init
 
 mklink "%HOMEPATH%\.gitconfig" "%HOMEPATH%\dotfiles\gitconfig"
 mklink "%HOMEPATH%\.gitignore_global" "%HOMEPATH%\dotfiles\gitignore_global"
